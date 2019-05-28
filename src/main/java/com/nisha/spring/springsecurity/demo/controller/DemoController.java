a5f2c0ec8afb386e5e0c69ded8903f77afa2a82a
Nisha Garg <14gargnisha@gmail.com>
2019-05-28 08:38:46 +0530

    Spring Security user roles with landing page

---
+++ src/main/java/com/nisha/spring/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.nisha.spring.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	@GetMapping("/")
+	public String showLanding() {
+		return "landing";
+	}
+	@GetMapping("/employees") 
+	public String showHome() {
+		return "home";
+	}
+}
